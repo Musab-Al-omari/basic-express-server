7c1b4176c7209f5256b64b18e10d4eafc27872e0
Musab-Al-omari <musabsomari@gmail.com>
2021-05-09 01:00:30 +0300

    fix test

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,12 +1,7 @@
 module.exports = (request, response, next) => {
-  if (request.query) {
-    let checked = request.query.name;
-    if (checked) {
-      next()
-    } else {
-      next('error')
-    }
+  if (request.query.name) {
+    next();
   } else {
-    next('error')
+    next('gg');
   }
 }
